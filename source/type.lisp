0d21b678fb47f634ae7a43165350e91bfe49da27
hikettei <hikettei@gmail.com>
2023-09-19 18:44:54 +0900

    update

--- source/type.lisp
+++ source/type.lisp
@@ -1,6 +1,7 @@
 
 (in-package :cl-const-generics)
 
+;; 構造体のSlotsを変える -> SegFault!!
 ;; 戦略: コンパイル前に必要なStructの名前全部コンパイル
 ;; Inline
 ;; (wrap-classみたいなマクロを作る
@@ -23,6 +24,29 @@
 ;; Compiler-macroでstruct + struct -> structを返す関数を作る + 構造体を入れ替え
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun symb (&rest inputs)
+    (intern (with-output-to-string (out) (dolist (sym inputs) (princ sym out)))))
+  
+  (defun symb-cache (&rest inputs)
+    (intern (with-output-to-string (out) (dolist (sym inputs) (princ sym out))) (find-package :cl-const-generics.subtypes)))
+  
+  (defun env-parameter-p (sym) (equal (aref (symbol-name sym) 0) #\&))
+  (defun get-params (list)
+    "Reading the given list, which is a form of arguments with defun, the function returns a list of symbols"
+    (delete-duplicates
+     (loop for i fixnum upfrom 0 below (length list)
+	   append
+	   (let ((sym (nth i list)))
+	     (typecase sym
+	       (symbol
+		(if (env-parameter-p sym)
+		    nil
+		    (list sym)))
+	       (list
+		(if (= (length sym) 2)
+		    (list (car sym))
+		    (get-params sym))))))))
+  
   (defun const-type-name (name &rest const)
     "Returns a function which returns a symbol like: Tensor(I<FIXNUM>=1, J<FIXNUM>=1)"
     #'(lambda (&rest args)
@@ -40,7 +64,7 @@
 	       `(,(car c) ,(second c) = ,arg))
 	 (list '|)|))))
   
-  (defun deftype-blueprint-form (name const struct-from
+  (defun deftype-blueprint-form (name const struct-from constructor-form
 				 &aux
 				   (f (apply #'const-type-name name const)))
     (declare (type symbol name))
@@ -54,16 +78,19 @@
 	     ;; douyatte kensaku subekika...
 	     (when (null (ignore-errors (find-class ',struct)))
 	       (defstruct (,struct
-			   (:constructor ,struct (,@(map 'list #'car const)))
-			   (:include ,struct-from)))
-	       
-	       )
+			   (:constructor ,struct (,@(map 'list #'car const) ,@(get-params constructor-form)))
+			   (:include ,struct-from))))
 	     ',struct)))))
 
 ;; TODO DOC Error Check
-(defmacro deftype-with-const (name (&rest const))
+;; TODO Checking Form
+;; TODO Extending Typep Forms
+(defmacro defstruct-generic (name (&rest const) &key (slots nil) (constructor nil))
   "
-## [macro] deftype-with-const
+## [macro] defstruct-generic
+
+const ... (name type ?)
+constructor ... must be &key (a 1) (b 1)
 "
 
   (assert (every #'(lambda (form) (= 2 (length form))) const)
@@ -72,26 +99,26 @@
   
   `(eval-when (:compile-toplevel :load-toplevel :execute)
      (defstruct (,name
-		 (:constructor ,(symb 'make-original- name) (,@(map 'list #'car const))))
+		 (:constructor
+		     ,(symb 'make-original- name)
+		     (,@(map 'list #'car const) ,@(get-params constructor))))
        ,@(loop for c in const
 	       collect
-	       (progn `(,(car c) ,(car c) :type ,(second c)))))
+	       (progn `(,(car c) ,(car c) :type ,(second c))))
+       ,@slots)
 
-     (defmacro ,(symb 'make- name) (,@(map 'list #'car const))
-       ;; [FIX]
+     ;; (PARAMETERS) MORE ARGS...
+     (defmacro ,(symb 'make- name) ((,@(map 'list #'car const)) ,@constructor)
        `(,(eval `(,',name ,,@(map 'list #'car const)))
-	 ,,@(map 'list #'car const)))
+	 ,,@(map 'list #'car const)
+	 ,,@(get-params constructor)))
      
      (defmacro ,name (,@(map 'list #'car const))
        (eval-when (:compile-toplevel :load-toplevel :execute)
-	 (let ((deftype-form (deftype-blueprint-form ',name ',const ',name)))
+	 (let ((deftype-form (deftype-blueprint-form ',name ',const ',name ',constructor)))
 	   (let ((type-identifier (eval (apply deftype-form (list ,@(map 'list #'car const))))))
 	     `',type-identifier))))))
 
-;; (the #.(Tensor 3) (make-tensor 3))
-;; rename: define-const-generic
-;; ((name type pred)) t=ignore
-(deftype-with-const Tensor
-    ((rank fixnum)))
-
+(deftype <const> (form)
+  `(and ,(eval form)))
 
--- source/typed-function.lisp
+++ source/typed-function.lisp
@@ -1,23 +1,25 @@
 
 (in-package :cl-const-generics)
 
+;; 再定義 ;; Aprops全部消す
+(defstruct-generic Tensor
+    ((rank fixnum))
+    :slots ((value 1))
+    :constructor (&key (value 0)))
 
-;; compiler-macroとThe!で型をUpdate
+(declaim (ftype (function ((<const> (Tensor 1))
+			   (<const> (Tensor 2)))
+			  (<const> (Tensor 3)))
+		adds-rank))
+(defun adds-rank (x y)
+  (make-tensor (3) :value (+ (tensor-value x) (tensor-value y))))
 
-(defmacro invite-type (function-name)
-  "
-## [macro]
-invite-type
+(defun test-function ()
+  (let ((x (make-tensor (1) :value 3))
+	(y (make-tensor (2) :value 4)))
+    (tensor-value (adds-rank x y))
 
-Attributes the ConstGenerics to the function.
-"
-  `(eval-when (:compile-toplevel :load-toplevel :execute)
-     (define-compiler-macro ',function-name (&whole form args)
-       
-       )))
+    ;;(adds-rank x (adds-rank x y))
+    ))
 
 
-
-(defun add-rank (x y)
-  (make-tensor
-   (+ (tensor-rank x) (tensor-rank y))))
